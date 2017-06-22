2baf4149b79e48dbabe5604b49082df7607dcb3e
Matt Jones <jonesmac82@Matts-MacBook-Pro.local>
2017-06-21 20:02:46 -0400

    wire up redux to main app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Store, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const initialState = {
+  locations: []
+};
+
+const store: Store<any> = createStore(rootReducer, initialState);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
